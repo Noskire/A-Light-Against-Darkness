224794e43ba7e7e19c51016a947504ae2ed0cdcc
Erikson Silacier <silacier2@hotmail.com>
2022-05-19 00:18:36 -0300

    Animations and Adjustments
    Animated two enemies and did little ajustments

--- src/Enemies/Enemy_Attack.gd
+++ src/Enemies/Enemy_Attack.gd
@@ -7,7 +7,7 @@ func _physics_process(_delta: float) -> void:
 		if is_instance_valid(player):
 			look_at(player.position)
 			target_pos = player.position - get_global_position()
-		if abs(target_pos.x) < 70 and abs(target_pos.y) < 70:
+		if abs(target_pos.x) < 110 and abs(target_pos.y) < 110:
 			state = PREPARE
 			anim_player.play("Prepare")
 		else:
